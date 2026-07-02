03130111bc4906fc1ee49e39d93f44fa0e1c9458
froggicat <166259685+froggicat@users.noreply.github.com>
2026-07-02 17:17:24 +0100

    Added csv with books and their authors

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ from models.book import Book
 from models.member import Member
 
 library = Library()
+
+#initialise library with a bunch of books and members
+#allow members to sign in + view or borrow or give back books
+#maybe differentiate between staff and regular members
+#where staff members can add or remove both books and regular members
+
